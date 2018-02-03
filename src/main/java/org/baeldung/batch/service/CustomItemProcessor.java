b70a1d25d075c03961e22ba7480835f7af006a4d
wanghaoming <whimmy@126.com>
2018-02-03 22:17:36 +0800

    first commit

---
+++ src/main/java/org/baeldung/batch/service/CustomItemProcessor.java
@@ -0,0 +1,13 @@
+package org.baeldung.batch.service;
+
+import org.baeldung.batch.model.Transaction;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomItemProcessor implements ItemProcessor<Transaction, Transaction> {
+
+    @Override
+    public Transaction process(Transaction item) {
+        System.out.println("Processing..." + item);
+        return item;
+    }
+}
