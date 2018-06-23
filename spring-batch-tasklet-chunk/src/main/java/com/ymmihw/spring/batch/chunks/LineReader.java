b1b9b29ae2a9aa322259603bdabb9e479dcbc43f
whimmy <whimmy@126.com>
2018-06-23 14:46:32 +0800

    copy code

---
+++ spring-batch-tasklet-chunk/src/main/java/com/ymmihw/spring/batch/chunks/LineReader.java
@@ -0,0 +1,37 @@
+package com.ymmihw.spring.batch.chunks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.item.ItemReader;
+import com.ymmihw.spring.batch.model.Line;
+import com.ymmihw.spring.batch.utils.FileUtils;
+
+public class LineReader implements ItemReader<Line>, StepExecutionListener {
+
+  private final Logger logger = LoggerFactory.getLogger(LineReader.class);
+  private FileUtils fu;
+
+  @Override
+  public void beforeStep(StepExecution stepExecution) {
+    fu = new FileUtils("taskletsvschunks/input/tasklets-vs-chunks.csv");
+    logger.debug("Line Reader initialized.");
+  }
+
+  @Override
+  public Line read() throws Exception {
+    Line line = fu.readLine();
+    if (line != null)
+      logger.debug("Read line: " + line.toString());
+    return line;
+  }
+
+  @Override
+  public ExitStatus afterStep(StepExecution stepExecution) {
+    fu.closeReader();
+    logger.debug("Line Reader ended.");
+    return ExitStatus.COMPLETED;
+  }
+}
---
+++ spring-batch-tasklet-chunk/src/test/java/com/ymmihw/spring/batch/chunks/ChunksIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.ymmihw.spring.batch.chunks;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.ymmihw.spring.batch.config.ChunksConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ChunksConfig.class)
+public class ChunksIntegrationTest {
+
+  @Autowired
+  private JobLauncherTestUtils jobLauncherTestUtils;
+
+  @Test
+  public void givenChunksJob_WhenJobEnds_ThenStatusCompleted() throws Exception {
+    JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+    Assert.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+  }
+}
